a091d0010718991ec83222fb45825de8ab78d765
axl0926 <song22092000@gmail.com>
2023-12-22 05:41:22 +0900

    fix:카테고리가 존재하지 않는 케이스  수정처

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
 
     let category: string[] = ["All"];
     response?.content.data.forEach((data: { [key: string]: any }) => {
-        if (!category.includes( data.liveCategoryValue)) {
-            category.push( data.liveCategoryValue);
+        if (data.liveCategoryValue !== "" && !category.includes(data.liveCategoryValue)) {
+            category.push(data.liveCategoryValue);
         }
     });
     category.push("카테고리 없음");
@@ -28,7 +28,7 @@ export default function Home() {
     return (
         <main className="w-[1260px] py-5">
             <div className="flex gap-2">
-                {category.map((category,i) => {
+                {category.map((category, i) => {
                     return (
                         <div key={i} className="bg-[rgba(46,48,51,.6)] rounded-[8px] p-[10px_20px] text-[#9da5b6] font-extrabold text-[20px] leading-[20px] hover:bg-[rgba(60,61,65,0.6)] cursor-pointer" onClick={() => setSelectedCategory(category)}>
                             {category}
@@ -39,8 +39,8 @@ export default function Home() {
             <div className="grid grid-cols-4">
                 {response?.content.data
                     .filter((data: { [key: string]: any }) => selectedCategory === "All" || data.liveCategoryValue === selectedCategory || (selectedCategory === "카테고리 없음" && data.liveCategoryValue == ""))
-                    .map((data: { [key: string]: any },i) => {
-                        console.log(response)
+                    .map((data: { [key: string]: any }, i) => {
+                        console.log(response);
                         return <VideoCardContainer data={data} key={i}></VideoCardContainer>;
                     })}
             </div>
