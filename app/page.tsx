91516d07005d72a41f91ffd9cb8e0f72beae632a
axl0926 <song22092000@gmail.com>
2023-12-23 22:27:05 +0900

    fix:방송 중인 모든 라이브를  받아오도록 수정

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,28 @@ import { useQuery } from "react-query";
 
 export default function Home() {
     const [selectedCategory, setSelectedCategory] = useState<string>("All");
-    type Response = { content: { data: { [key: string]: any }[] } };
-    const response = useQuery<Response, Error>({
+
+    type Lives = any[];
+    const { data, status } = useQuery<Lives, Error>({
         queryKey: [`fetch`],
         queryFn: async () => {
-            const res = await fetch(`/api/getLives`);
-            return res.json();
+            const response = await fetch(`/api/getLives`);
+            return response.json();
         },
         refetchInterval: 60000,
         refetchOnWindowFocus: false,
-    }).data;
+    });
 
     let category: string[] = ["All"];
-    response?.content.data.forEach((data: { [key: string]: any }) => {
-        if (data.liveCategoryValue !== "" && !category.includes(data.liveCategoryValue)) {
-            category.push(data.liveCategoryValue);
-        }
-    });
-    category.push("카테고리 없음");
+    if (status === "success") {
+        data.forEach((data: { [key: string]: any }) => {
+            if (data.liveCategoryValue !== "" && !category.includes(data.liveCategoryValue)) {
+                category.push(data.liveCategoryValue);
+            }
+        });
+    }
 
+    category.push("카테고리 없음");
     return (
         <main className="w-[1260px] py-5">
             <div className="flex gap-2 flex-wrap">
@@ -37,11 +40,12 @@ export default function Home() {
                 })}
             </div>
             <div className="grid grid-cols-4">
-                {response?.content.data
-                    .filter((data: { [key: string]: any }) => selectedCategory === "All" || data.liveCategoryValue === selectedCategory || (selectedCategory === "카테고리 없음" && data.liveCategoryValue == ""))
-                    .map((data: { [key: string]: any }, i) => {
-                        return <VideoCardContainer data={data} key={i}></VideoCardContainer>;
-                    })}
+                {status === "success" &&
+                    data
+                        .filter((data: { [key: string]: any }) => selectedCategory === "All" || data.liveCategoryValue === selectedCategory || (selectedCategory === "카테고리 없음" && data.liveCategoryValue == ""))
+                        .map((data: { [key: string]: any }, i) => {
+                            return <VideoCardContainer data={data} key={i}></VideoCardContainer>;
+                        })}
             </div>
         </main>
     );
--- pages/api/getLives.tsx
+++ pages/api/getLives.tsx
@@ -1,8 +1,31 @@
-import { NextApiRequest,NextApiResponse } from "next/types";
+import { NextApiRequest, NextApiResponse } from "next/types";
+
+const url = "https://api.chzzk.naver.com/service/v1/lives?";
+
+type Response = {
+    code: number;
+    message: string;
+    content: {
+        data: any[];
+        page: { next: { concurrentUserCount: number; liveId: number } };
+        size: number;
+    };
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const response = await fetch(`https://api.chzzk.naver.com/service/v1/lives?`);
-    const data = await response.json();
+    let data = [];
+    let nextUrl = url;
+
+    let responseData: Response = await (await fetch(nextUrl)).json();
+
+    do {
+        data.push(...responseData.content.data);
+
+        if (responseData.content.size > 0) {
+            const next = responseData.content.page.next;
+            nextUrl = `${url}&concurrentUserCount=${next.concurrentUserCount}&liveId=${next.liveId}`;
+            responseData = await (await fetch(nextUrl)).json();
+        }
+    } while (responseData && responseData.content.size > 0);
     res.status(200).json(data);
 }
-
